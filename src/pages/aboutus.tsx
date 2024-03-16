eba6f16cc70e5580dfa050d73201c810c6f74c02
DhanashreeS198 <dhanujd198@gmail.com>
2024-03-16 12:37:07 +0530

    add files

---
+++ src/pages/aboutus.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+const AboutUs = () => {
+  return (
+    <div className="flex flex-col lg:flex-row justify-center items-start lg:items-center min-h-screen pt-5 lg:pt-0 pb-10 px-4 bg-purple-200">
+    {/* About Us Section */}
+    <div className="w-full lg:w-1/2 pb-12 lg:pb-0 pr-4">
+      <h2 className="text-3xl font-bold mb-4">About Us</h2>
+      <p className="text-lg text-gray-700">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+        ullamcorper mattis tellus. Fusce id sapien nec libero porta tempor.
+        Integer vitae justo vel justo aliquet fermentum. Nulla facilisi.
+        Phasellus dictum augue nec arcu vehicula, ac suscipit mauris molestie.
+        Vestibulum hendrerit ex in ante laoreet, id efficitur eros vehicula.
+        Vivamus pretium orci eget nisi tincidunt fermentum. In hac habitasse
+        platea dictumst. Maecenas ultrices bibendum ipsum, in mattis justo
+        tincidunt sed. Donec non tortor nec eros vehicula aliquet. Nullam id
+        varius mi, sit amet sollicitudin neque. Proin semper felis ac turpis
+        consectetur, nec tincidunt urna aliquet. In id sodales sapien, vel
+        suscipit felis. Integer a risus in enim vestibulum laoreet. Mauris
+        tempus sapien sed dictum tempus. Cras sit amet nisi vitae elit
+        suscipit pharetra. Sed sit amet lectus sed enim pulvinar consectetur.
+        Aliquam luctus suscipit libero nec molestie. Nam in accumsan purus.
+        Nam tincidunt ultricies arcu non luctus. Donec blandit libero ut lorem
+        pharetra, sed bibendum lorem luctus. Mauris eget ipsum non leo
+        vestibulum venenatis id ac sapien.
+      </p>
+    </div>
+  
+    {/* Video Section */}
+    <div className="w-full lg:w-1/2 p-8">
+      <div className="aspect-w-16 lg:aspect-w-20 aspect-h-9 lg:aspect-h-50 h-[350px] rounded-xl overflow-hidden">
+        <iframe
+          className="w-full h-full"
+          src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+          title="About Us Video"
+          allowFullScreen
+        ></iframe>
+      </div>
+    </div>
+  </div>
+  
+  
+  );
+};
+
+export default AboutUs;
---
+++ src/pages/home.tsx
@@ -0,0 +1,12 @@
+import MySlider from '@/Components/slider'
+import React from 'react'
+
+const home = () => {
+  return (
+    <>
+    <MySlider/>
+    </>
+  )
+}
+
+export default home
